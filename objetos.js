5e46edac0114a529fa89a224399a284368fb92b5
alfred04joseph <josephr.041999@gmail.com>
2023-06-10 00:28:52 -0600

    subiendo objetos

---
+++ objetos.js
@@ -0,0 +1,10 @@
+let album = {
+    genero: " Rock ",
+    nameMusic: " Whispers In The Dark ",
+    years: 1995
+    };
+    console.log(album[genero]);
+    console.log(album[nameMusic]);
+   console.log(album[years]);
+   
+   
